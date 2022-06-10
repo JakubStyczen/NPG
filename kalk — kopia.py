18b9e092490b3150694383ce02dec6c1bec57276
JakubStyczen <66252957+JakubStyczen@users.noreply.github.com>
2022-06-10 08:34:28 +0200

    Add files via upload

--- kalk — kopia.py
+++ kalk — kopia.py
@@ -1,5 +1,6 @@
 import re
 import math
+import sys
 #re do dokumentacji panowie poczytajcie cos i napiszcie o tym imporcie, mozecie opisac jak dziala ten patern ponziej  
 #math do logarytmu o dowolnej podstawie 
 
@@ -9,35 +10,35 @@ pattern = re.compile(r'(^[-]?\d+\.?\d*[+-]\d+\.?\d*j$|^[-]?\d+\.?\d*j?$)')
 
 
 
-#testy to powyzszego regex
+# #testy to powyzszego regex
 
-test = '''
--2
-2-2j
--2-2j
--2.2
--2.2-2.2j
--0
-2
-2+
-+2
-2j
-222j
-222
-2.222
-2.222j
-2+2j
-2.2+2.2j
-22+22
-j
-asdas
-sad+sad
-'''
-raw = test.split('\n')
-matches = pattern.match(test)
-for match in raw:
-   if pattern.match(match):
-       print(match)
+# test = '''
+# -2
+# 2-2j
+# -2-2j
+# -2.2
+# -2.2-2.2j
+# -0
+# 2
+# 2+
+# +2
+# 2j
+# 222j
+# 222
+# 2.222
+# 2.222j
+# 2+2j
+# 2.2+2.2j
+# 22+22
+# j
+# asdas
+# sad+sad
+# '''
+# raw = test.split('\n')
+# matches = pattern.match(test)
+# for match in raw:
+#    if pattern.match(match):
+#        print(match)
 
 
 
@@ -141,50 +142,64 @@ if __name__ == '__main__':
         print('\n------------------------------\n'+
         'Witaj w poteznym kalkulatorze TS, zostaniesz poproszony o wybor dzialnia ' +
         'oraz podanie argumrtow dzialnia.\nLiczby zespolone nalezy wpisywac w formacie X+Yj!\n'+
+        'CTRL + C aby wyjść\n'+
         '------------------------------\n')
         while True:
-            #Przyjmowanie danych od uzytkownika
-            operator = validatioInputString('Wybierz dzialanie +(1) -(2) *(3) /(4) ^(5) \u221A(6) log(7): ', operators.keys())
-            print()
-            print(operators[operator][1])
-            print()
-            ans1, ans2  = None, None
-            #wybor wynikow do uzywanych argumentow
-            if store.size_of() > 0:
-                prev = validatioInputString('Czy chcesz użyc poprzecnich wynikow? Y/N ', ['Y', 'N'])
-                if prev == 'Y':
-                    for idx, sol in enumerate(store):
-                        print(f'{idx+1}.', end = " ")
-                        printForm(sol)
-                    ans1 = int(validatioInputString('Podaj numer wyniku do 1 argumentu lub 0 jeżeli nie chcesz nic wpisywać: ', [str(x) for x in range(store.size_of() + 1)]))
-                    ans2 = int(validatioInputString('Podaj numer wyniku do 2 argumentu lub 0 jeżeli nie chcesz nic wpisywać: ', [str(x) for x in range(store.size_of() + 1)]))
-                    ans1 = store.operations[ans1 - 1] if ans1 != 0 else None
-                    ans2 = store.operations[ans2 - 1] if ans2 != 0 else None
-            #wyprowadzenie argumentow         
-            if ans1 == None:
-                arg1 = complex(isValidDigit('Podaj 1 arg: ', pattern))
-            else: 
-                arg1 = ans1
-            if ans2 == None:    
-                arg2 = complex(isValidDigit('Podaj 2 arg: ', pattern))
-            else: 
-                arg2 = ans2
-            #blok sprawdzający warunki pierwiastka, pierwiastka n stopnia
-            while not arg2 and operator == '4' or operator == '6':
-                print("Nie dziel przez 0!!!") if operator == '4' else print("Pierwiastek nie może być stopnia 0!!!") 
-                arg2 = complex(isValidDigit('Podaj 2 arg: ', pattern))
-            while not (arg1.real > 0 and arg1.real != 1) and arg2.real <= 0:
-                if not (arg1.real > 0 and arg1.real != 1):
-                    print("Podstawa musi być dodatnia i różna od 1!!!")
-                    arg1 = complex(isValidDigit('Podaj 1 arg: ', pattern))
-                else:
-                    print("Liczba logarytmowana musi być dodatnia!!!")
-                    arg2 = complex(isValidDigit('Podaj 1 arg: ', pattern))
+            except_flag = None
+            while not except_flag:
+                try:
+                    #Przyjmowanie danych od uzytkownika
+                    operator = validatioInputString('Wybierz dzialanie +(1) -(2) *(3) /(4) ^(5) \u221A(6) log(7): ', operators.keys())
+                    print()
+                    print(operators[operator][1])
+                    print()
+                    ans1, ans2  = None, None
+                    #wybor wynikow do uzywanych argumentow
+                    if store.size_of() > 0:
+                        prev = validatioInputString('Czy chcesz użyc poprzecnich wynikow? Y/N ', ['Y', 'N'])
+                        if prev == 'Y':
+                            for idx, sol in enumerate(store):
+                                print(f'{idx+1}.', end = " ")
+                                printForm(sol)
+                            ans1 = int(validatioInputString('Podaj numer wyniku do 1 argumentu lub 0 jeżeli nie chcesz nic wpisywać: ', [str(x) for x in range(store.size_of() + 1)]))
+                            ans2 = int(validatioInputString('Podaj numer wyniku do 2 argumentu lub 0 jeżeli nie chcesz nic wpisywać: ', [str(x) for x in range(store.size_of() + 1)]))
+                            ans1 = store.operations[ans1 - 1] if ans1 != 0 else None
+                            ans2 = store.operations[ans2 - 1] if ans2 != 0 else None
+                    #wyprowadzenie argumentow         
+                    if ans1 == None:
+                        arg1 = complex(isValidDigit('Podaj 1 arg: ', pattern))
+                    else: 
+                        arg1 = ans1
+                    if ans2 == None:    
+                        arg2 = complex(isValidDigit('Podaj 2 arg: ', pattern))
+                    else: 
+                        arg2 = ans2
+        
+                    #blok sprawdzający warunki pierwiastka, pierwiastka n stopnia
+                    while arg2 == 0 and (operator == '4' or operator == '6'):
+                        print("Nie dziel przez 0!!!") if operator == '4' else print("Pierwiastek nie może być stopnia 0!!!") 
+                        arg2 = complex(isValidDigit('Podaj 2 arg: ', pattern))
 
 
+                    while operator == '7' and (not (arg1.real > 0 and arg1.real != 1) or arg2.real <= 0):
+                        if not (arg1.real > 0 and arg1.real != 1):
+                            print("Podstawa musi być dodatnia i różna od 1!!!")
+                            arg1 = complex(isValidDigit('Podaj 1 arg: ', pattern))
+                            arg2 = complex(isValidDigit('Podaj 2 arg: ', pattern))
+                        else:
+                            print("Liczba logarytmowana musi być dodatnia!!!")
+                            arg1 = complex(isValidDigit('Podaj 1 arg: ', pattern))
+                            arg2 = complex(isValidDigit('Podaj 2 arg: ', pattern))
+                
 
-            #dodanie do kolejki wyniku
-            solution = calc(operator, arg1, arg2)
+
+                #dodanie do kolejki wyniku
+                    solution = calc(operator, arg1, arg2)
+                    except_flag = True
+                except OverflowError:
+                    print("\nZbyt duży format liczb. Nasz przepotężny kalkulator nie obsługuje aż takich :)\n")
+                    except_flag = None
+
             if store.size_of() == 10:
                 store.dequeue()
                 store.enqueue(solution)
@@ -207,5 +222,7 @@ if __name__ == '__main__':
                         printForm(sol)
                         
     except KeyboardInterrupt:
-        print("Wyjscie z programu")
+        print("\nWyjscie z programu\n")
+        sys.exit(0)
+    
 
